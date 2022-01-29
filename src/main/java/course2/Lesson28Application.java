58afbdcce9c52415e6deb7647dff20f84c342cf7
Елена <etretjakowa@yandex.ru>
2022-01-29 15:06:22 +0700

    ДЗ 2_8

---
+++ src/main/java/course2/Lesson28Application.java
@@ -0,0 +1,13 @@
+package course2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson28Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson28Application.class, args);
+    }
+
+}
---
+++ src/test/java/course2/Lesson28ApplicationTests.java
@@ -0,0 +1,13 @@
+package course2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson28ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
